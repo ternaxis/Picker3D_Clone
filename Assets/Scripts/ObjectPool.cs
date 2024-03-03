455eeb8f3a292940d2413de76ea30f1e1a9e1152
eren <ternaxis@outlook.com>
2024-03-03 19:04:53 +0300

    feat: game view configured (compared to original game)

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,18 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+//unity generic object pool
 
-public class ObjectPool : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
